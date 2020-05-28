3b89b4c18bc01ef92e2930e719b5ad077003175e
Raul <raulsantos.comic@gmail.com>
2020-05-28 14:53:48 -0300

    Exercício 1 - Aula 3

---
+++ Aula 3/Exercício 1/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    
+    $("#clear").click(function () {
+        $("#nameInput").val("");
+        $("#inputEmail").val("");
+        $("#inputPassword").val("");
+    })
+});
